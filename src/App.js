addf3c042375ee23b28505446c5ea5bb0c90d28c
Mac_Developer <araozangel842@gmail.com>
2023-02-26 18:05:57 +0200

    Add trailer player component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from './layout/Layout';
 import { Route, Routes } from 'react-router-dom';
 import Home from './views/Home';
 import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
 
 function App() {
   const [movies, setMovies] = React.useState();
@@ -29,6 +30,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home movies={movies} />} />
+            <Route path="/Trailer/:ytTrailerId" element={<Trailer />} />
         </Route>
       </Routes>
     </div>
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,35 +1,52 @@
-import React from 'react'
-import './Hero.css'
-import Carousel from 'react-material-ui-carousel'
-import { Paper } from '@mui/material'
+import React from 'react';
+import './Hero.css';
+import Carousel from 'react-material-ui-carousel';
+import { Paper } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
-function Hero({movies}) {
+function Hero({ movies }) {
   return (
-    <div className='movie-carousel-container'>
+    <div className="movie-carousel-container">
       <Carousel>
         {movies?.map((movie) => {
-          return(
+          return (
             <Paper key={movie.id}>
-            <div className="movie-card-container">
-              <div className="movie-card" style={{
-                "--img": `url(${movie.backdrops[0]})`
-              }}>
-                <div className="movie-detail">
-                  <div className="movie-poster">
-                     <img src={movie.poster} alt={movie.title} />
-                  </div>
-                  <div className="movie-title">
-                    <h1>{movie.title}</h1>
+              <div className="movie-card-container">
+                <div
+                  className="movie-card"
+                  style={{
+                    '--img': `url(${movie.backdrops[0]})`,
+                  }}
+                >
+                  <div className="movie-detail">
+                    <div className="movie-poster">
+                      <img src={movie.poster} alt={movie.title} />
+                    </div>
+                    <div className="movie-title">
+                      <h1>{movie.title}</h1>
+                    </div>
+                    <div className="movie-buttons-container">
+                      <Link
+                        to={`/Trailer/${movie.trailerLink.substring(
+                          movie.trailerLink.length - 11
+                        )}`}
+                      >
+                        <div className="play-button-icon-container">
+                          <FontAwesomeIcon icon={faCirclePlay} className="play-button-icon" />
+                        </div>
+                      </Link>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </Paper>
-          )
+            </Paper>
+          );
         })}
       </Carousel>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
