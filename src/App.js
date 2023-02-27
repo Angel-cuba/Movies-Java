aee6e03cd0a1af875e4497196e532b8dc24f4639
Mac_Developer <araozangel842@gmail.com>
2023-02-27 21:51:29 +0200

    Fetch movie details and passing data as props

--- src/App.js
+++ src/App.js
@@ -6,15 +6,31 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './views/Home';
 import Header from './components/header/Header';
 import Trailer from './components/trailer/Trailer';
+import Reviews from './views/Reviews';
 
 function App() {
   const [movies, setMovies] = React.useState();
-  console.log('🚀 ~ file: App.js:6 ~ App ~ movies:', movies);
+  const [movie, setMovie] = React.useState();
+  const [reviews, setReviews] = React.useState();
+
 
   const getMovies = async () => {
     try {
       const response = await api.get('/api/v1/movies');
       setMovies(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getMovieData = async (movieId) => {
+    try {
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+      setMovie(response.data);
+      if (response.data.reviewIds.length > 0) {
+        console.log(response.data.reviewIds);
+        setReviews(response.data.reviewIds);
+      }
     } catch (error) {
       console.log(error);
     }
@@ -26,11 +42,22 @@ function App() {
 
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home movies={movies} />} />
-            <Route path="/Trailer/:ytTrailerId" element={<Trailer />} />
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer />} />
+          <Route
+            path="/Reviews/:movieId"
+            element={
+              <Reviews
+                getMovieData={getMovieData}
+                movie={movie}
+                reviews={reviews}
+                setReviews={setReviews}
+              />
+            }
+          />
         </Route>
       </Routes>
     </div>
